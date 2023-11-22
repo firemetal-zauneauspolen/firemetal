ed9f29b633904a5f0267380da00a1c7f89e3b01d
ilisiecki <igor.fox.lisiecki@gmail.com>
2023-11-22 12:07:52 +0100

    Fix not found redirect url and translate

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -5,12 +5,9 @@ export default function NotFoundPage() {
     <main className="-mt-36 flex min-h-screen items-center justify-center px-2">
       <div className="flex flex-col items-center text-center">
         <Spinner size="lg" className="pb-4" aria-label="Loding..." />
-        <p>Strona o podanym adresie nie istnieje. </p>
-        <p>Zostaniesz automatycznie przekierowany za kilka sekund...</p>
-        <meta
-          http-equiv="refresh"
-          content={`3;url=https://${process.env.SITE_URL}`}
-        />
+        <p>Die Webseite unter der angegebenen Adresse existiert nicht. </p>
+        <p>Du wirst in wenigen Sekunden automatisch weitergeleitet...</p>
+        <meta http-equiv="refresh" content={`3;url=${process.env.SITE_URL}`} />
       </div>
     </main>
   );
